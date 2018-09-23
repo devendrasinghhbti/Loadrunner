092241c38be5e0908c5b0872b03867332e04837d
devendrasinghhbti <34220260+devendrasinghhbti@users.noreply.github.com>
2018-09-23 23:04:10 +0530

    Update Batchcommand.bat

--- Batchcommand.bat
+++ Batchcommand.bat
@@ -1,2 +1,3 @@
 set JMETER_HOME=G:\apache-jmeter-4.0
 G:\apache-jmeter-4.0\bin\jmeter.bat -Jjmeter.save.saveservice.output_format=xml -n -t G:\apache-jmeter-4.0\bin\Test.jmx -l G:\apache-jmeter-4.0\bin\Test.jtl
+echo "test run"
